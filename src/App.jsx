346227913f0969611d357d02240669ecfcb3a762
Ridhi Chhajer <ridhichhajer11@gmail.com>
2023-09-19 11:25:25 +0530

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-import DocSimp from "./page/DocSimp/DocSimp";
-import TG from "./page/TG/TG";
-import EL from "./page/EL/EL";
-import './app.scss'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import DocSimp from './page/DocSimp/DocSimp';
+import TG from './page/TG/TG';
+import EL from './page/EL/EL';
+import './app.scss';
+
 function App() {
     return (
-        <div className='App'>
-            {/* <DocSimp/> */}
-            <EL/>
-            {/* <TG/> */}
-        </div>
+        <Router>
+            <div className='App'>
+                <Routes>
+                    <Route exact path='/' element={<DocSimp />} />
+                    <Route path='/tg' element={<TG />} />
+                    <Route path='/el' element={<EL />} />
+                </Routes>
+            </div>
+        </Router>
     );
 }
 
